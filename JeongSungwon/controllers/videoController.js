d6035fd20b73b8eb5c9ccc7262c36a33071388ef
sungwon <chsw93@gmail.com>
2020-12-01 20:26:10 +0900

    #2.25 More Controlers

--- JeongSungwon/controllers/videoController.js
+++ JeongSungwon/controllers/videoController.js
@@ -1,5 +1,5 @@
 import {videos} from "../db"
-
+import routes from "../routes"
 export const home = (req, res) =>{
   res.render("home", { pageTitle: "Home", videos } );
 } 
@@ -10,14 +10,21 @@ export const search = (req, res) => {
   res.render("search", { pageTitle: "Search", searchingBy, videos});
 }
 
-export const upload = (req, res) =>
+export const getUpload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
+export const postUpload = (req, res) =>{
+  const{
+    body:{ file, title, description }
+  } = req;
+  // To DO: Upload and save video
+  res.redirect(routes.videoDetail(321321))
+};
 
-  export const videoDetail = (req, res) =>
-  res.render("videoDetail", { pageTitle: "Video Detail" });
+export const videoDetail = (req, res) =>
+res.render("videoDetail", { pageTitle: "Video Detail" });
 
-  export const editVideo = (req, res) =>
-  res.render("editVideo", { pageTitle: "Edit Video" });
+export const editVideo = (req, res) =>
+res.render("editVideo", { pageTitle: "Edit Video" });
 
-  export const deleteVideo = (req, res) =>
-  res.render("deleteVideo", { pageTitle: "Delete Video" });
+export const deleteVideo = (req, res) =>
+res.render("deleteVideo", { pageTitle: "Delete Video" });
