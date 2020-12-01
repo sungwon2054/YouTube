e766600b9b418c28ca0391b7c9b89f6fafae421a
sungwon <chsw93@gmail.com>
2020-12-01 15:34:46 +0900

    #2.21 Home Controller

---
+++ JeongSungwon/db.js
@@ -0,0 +1,51 @@
+export const videos = [
+  {
+    id:321321,
+    title: "video awesome",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "Nicolas",
+      email: "nico@las.com"
+    }
+  },
+  {
+    id:11111,
+    title: "video super",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "Nicolas",
+      email: "nico@las.com"
+    }
+  },
+  {
+    id:22222,
+    title: "video nice",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "Nicolas",
+      email: "nico@las.com"
+    }
+  },
+  {
+    id:33333,
+    title: "video perfect",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    creator: {
+      id: 121212,
+      name: "Nicolas",
+      email: "nico@las.com"
+    }
+  }
+
+]
