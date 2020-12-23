116b5ce38d6b52c4b7dbb00e59bcb97a2de32ce1
sungwon <chsw93@gmail.com>
2020-12-23 23:36:56 +0900

    #4.2 Styles with Webpack part Two

--- JeongSungwon/webpack.config.js
+++ JeongSungwon/webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
-const ExtractCSS = require('mini-css-extract-plugin');
+const autoprefixer = require('autoprefixer');
+const ExtractCSS = require('extract-text-webpack-plugin');
 
 const MODE = process.env.WEBPACK_ENV;
 const ENTRY_FILE = path.resolve(__dirname, 'assets', 'js', 'main.js');
 const OUTPUT_DIR = path.join(__dirname, 'static');
-
 const config = {
   entry: ENTRY_FILE,
   mode: MODE,
@@ -17,8 +17,13 @@ const config = {
             loader: 'css-loader',
           },
           {
-            loader: 'postcss-loader'
-          }
+            loader: 'postcss-loader',
+            options: {
+              plugin() {
+                return [autoprefixer({ browsers: 'cover 99.5%' })];
+              },
+            },
+          },
           {
             loader: 'sass-loader',
           },
@@ -28,8 +33,9 @@ const config = {
   },
   output: {
     path: OUTPUT_DIR,
-    filename: '[name].[format]',
+    filename: '[name].js',
   },
+  plugins: [new ExtractCSS('styles.css')],
 };
 
 module.exports = config;
