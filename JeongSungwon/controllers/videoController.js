677d826551e1e32edece8ffcf2da89f5f4a37559
sungwon <chsw93@gmail.com>
2020-12-24 23:20:25 +0900

    #7.4 Protecting Video Routes

--- JeongSungwon/controllers/videoController.js
+++ JeongSungwon/controllers/videoController.js
@@ -52,7 +52,6 @@ export const videoDetail = async (req, res) => {
   } = req;
   try {
     const video = await Video.findById(id).populate('creator');
-    console.log(video);
     res.render('videoDetail', { pageTitle: video.title, video: video });
   } catch (error) {
     res.redirect(routes.home);
@@ -65,7 +64,11 @@ export const getEditVideo = async (req, res) => {
   } = req;
   try {
     const video = await Video.findById(id);
-    res.render('editVideo', { pageTitle: `Edit ${video.title}`, video });
+    if (video.creator !== req.user.id) {
+      throw Error();
+    } else {
+      res.render('editVideo', { pageTitle: `Edit ${video.title}`, video });
+    }
   } catch (error) {
     res.redirect(routes.home);
   }
@@ -89,7 +92,12 @@ export const deleteVideo = async (req, res) => {
     params: { id },
   } = req;
   try {
-    await Video.findByIdAndRemove({ _id: id });
+    const video = await Video.findById(id);
+    if (video.creator !== req.user.id) {
+      throw Error();
+    } else {
+      await Video.findOneAndRemove({ _id: id });
+    }
   } catch (error) {
     console.log(error);
   }
