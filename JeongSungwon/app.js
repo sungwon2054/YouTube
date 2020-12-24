286d8f576eb5cb95ef870ca176cd99c427a38dcd
sungwon <chsw93@gmail.com>
2020-12-24 15:29:58 +0900

    #6.5 MongoStore and Middlewares

--- JeongSungwon/app.js
+++ JeongSungwon/app.js
@@ -4,7 +4,9 @@ import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import passport from 'passport';
+import mongoose from 'mongoose';
 import session from 'express-session';
+import MongoStore from 'connect-mongo';
 import { localsMiddleware } from './middlewares';
 import routes from './routes';
 import userRouter from './routers/userRouter';
@@ -14,6 +16,8 @@ import globalRouter from './routers/globalRouter';
 import './passport';
 
 const app = express();
+
+const CokieStore = MongoStore(session);
 
 console.log(process.env.COOKIE_SECRET);
 
@@ -30,6 +34,7 @@ app.use(
     secret: process.env.COOKIE_SECRET,
     resave: true,
     saveUninitialized: false,
+    store: new CokieStore({ mongooseConnection: mongoose.connection }),
   })
 );
 app.use(passport.initialize());
