e9c2760a975eb65a44ed46acc7c11e4c792f9d16
sungwon <chsw93@gmail.com>
2020-12-24 19:39:10 +0900

    #6.11 Facebook Login Part Two

--- JeongSungwon/passport.js
+++ JeongSungwon/passport.js
@@ -26,7 +26,9 @@ passport.use(
     {
       clientID: process.env.FB_ID,
       clientSecret: process.env.FB_SECRET,
-      callbackURL: `http://localhost:4000${routes.facebookCallback}`,
+      callbackURL: `https://red-dragonfly-100.loca.lt/${routes.facebookCallback}`,
+      profileFields: ['id', 'displayName', 'photos', 'email'],
+      scope: ['public_profile', 'email'],
     },
     facebookLoginCallback
   )
