2f712cb9b2c27415c269128a7ace45a45946410e
sungwon <chsw93@gmail.com>
2020-12-24 23:06:44 +0900

    #7.1 Edit User Password

--- JeongSungwon/routers/userRouter.js
+++ JeongSungwon/routers/userRouter.js
@@ -3,14 +3,16 @@ import routes from '../routes';
 import {
   users,
   userDetail,
-  editProfile,
+  getEditProfile,
   changePassword,
+  postEditProfile,
 } from '../controllers/userController';
-import { onlyPrivate } from '../middlewares';
+import { onlyPrivate, uploadAvatar } from '../middlewares';
 
 const userRouter = express.Router();
 
-userRouter.get(routes.editProfile, onlyPrivate, editProfile);
+userRouter.get(routes.editProfile, onlyPrivate, getEditProfile);
+userRouter.post(routes.editProfile, onlyPrivate, uploadAvatar, postEditProfile);
 userRouter.get(routes.changePassword, onlyPrivate, changePassword);
 userRouter.get(routes.userDetail(), userDetail);
 
