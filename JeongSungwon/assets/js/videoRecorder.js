0d5e735cd0c531b3ef4769391e1bd5819fed3da2
sungwon <chsw93@gmail.com>
2020-12-25 07:25:46 +0900

    #9.1 Recording Video part One

--- JeongSungwon/assets/js/videoRecorder.js
+++ JeongSungwon/assets/js/videoRecorder.js
@@ -2,7 +2,19 @@ const recorderContainer = document.getElementById('jsRecordContainer');
 const recordBtn = document.getElementById('jsRecordBtn');
 const videoPreview = document.getElementById('jsVideoPreview');
 
-const startRecording = async () => {
+let streamObject;
+
+const handleVideoData = (event) => {
+  console.log(event);
+};
+
+const startRecording = () => {
+  const videoRecorder = new MediaRecorder(streamObject);
+  videoRecorder.start();
+  videoRecorder.addEventListener('dataavailable', handleVideoData);
+};
+
+const getVideo = async () => {
   try {
     const stream = await navigator.mediaDevices.getUserMedia({
       audio: true,
@@ -13,12 +25,13 @@ const startRecording = async () => {
     videoPreview.play();
   } catch (error) {
     recordBtn.innerHTML = '☹️ Cant record';
-    recordBtn.removeEventListener('click', startRecording);
+  } finally {
+    recordBtn.removeEventListener('click', getVideo);
   }
 };
 
 function init() {
-  recordBtn.addEventListener('click', startRecording);
+  recordBtn.addEventListener('click', getVideo);
 }
 
 if (recorderContainer) {
